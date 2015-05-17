2f7263311cec2d15b46e40b04a477751e7e3f1ab
Risack Kevin <Kevin@MacBook-Pro-van-Kevin.local>
2015-05-17 14:04:23 +0200

    Added ALL McDonalds Establishments

--- MAD-Project2015/app/src/main/java/be/howest/nmct/mad_project2015/admin/McDonaldAdmin.java
+++ MAD-Project2015/app/src/main/java/be/howest/nmct/mad_project2015/admin/McDonaldAdmin.java
@@ -113,6 +113,116 @@ public final class McDonaldAdmin {
 
         mc = new McDonald("McDo RONSE/RENAIX", "César Snoecklaan 51", "9600", "Ronse", "Oost-Vlaanderen", new LatLng(50.745900,3.586187));
         listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo FROYENNES (Drive)", "Rue de Maire 4", "7503", "Froyennes", "Henegouwen", new LatLng(50.616456,3.375071));
+        listMcDonalds.add(mc);
+                
+        mc = new McDonald("McDo GOSSELIES (Drive)", "Rue Tahon 16", "6041", "Gosselies", "Henegouwen", new LatLng(50.464042,4.436874));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo LA LOUVIERE (Drive)", "Rue de la Franco Belge 0", "7100", "La Louvière", "Henegouwen", new LatLng(50.486259,4.208358));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo MARCINELLE (Drive)", "Route de Philippeville 238", "6001", "Marcinelles", "Henegouwen", new LatLng(50.400396,4.452476));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo MONS (In-Store)", "Grand Place 39", "7000", "Bergen", "Henegouwen", new LatLng(50.453797,3.952336));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo MOUSCRON (Drive)", "Rue (Raymond) Beaucarne 12", "7700", "Moeskroen", "Henegouwen", new LatLng(50.744789,3.238065));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo SOIGNIES (Drive)", "Rue de Steenkerque 32", "7060", "Soignies", "Henegouwen", new LatLng(50.582500,4.071244));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo WASMÜEL-QUAREGNON (Drive)", "Rue Mouzin 133", "7390", "Wasmuel-Quaregnon", "Henegouwen", new LatLng(50.440992,3.848010));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo CHARLEROI (Drive)", "Grand'Rue 48", "6000", "Charleroi", "Henegouwen", new LatLng(50.421744,4.460607));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo ALLEUR (Drive)", "Avenue du Progès 20", "4432", "Alleur", "Luik", new LatLng(50.672047,5.529257));
+        listMcDonalds.add(mc);
+                
+        mc = new McDonald("McDo EUPEN (Drive)", "Herbesthalerstrasse 128", "4700", "Eupen", "Luik", new LatLng(50.638669,6.016708));
+        listMcDonalds.add(mc);
+                
+        mc = new McDonald("McDo FLERON (Drive)", "Rue de la Clef 52", "4620", "Fleron", "Luik", new LatLng(50.617693,5.694958));
+        listMcDonalds.add(mc);
+                
+        mc = new McDonald("McDo JEMEPPE (Drive)", "Rue Renkin Sualem 0", "4101", "Jemeppe", "Luik", new LatLng(50.617323, 5.498547));
+        listMcDonalds.add(mc);
+                        
+        mc = new McDonald("McDo LIEGE (In-Store)", "Place de la Rép. Française 31", "4000", "Luik", "Luik", new LatLng(50.643251, 5.572366));
+        listMcDonalds.add(mc);
+                        
+        mc = new McDonald("McDo VERVIERS (Drive)", "Rue de la Station 8/27", "4800", "Verviers", "Luik", new LatLng(50.588754, 5.849415));
+        listMcDonalds.add(mc);
+                        
+        mc = new McDonald("McDo WAREMME (Drive)", "Rue Hyacinthe Docquier 173", "4300", "Waremme", "Luik", new LatLng(50.695641, 5.276123));
+        listMcDonalds.add(mc); 
+                        
+        mc = new McDonald("McDo WATERLOO (Drive)", "Leuvensesteenweg 2", "1410", "Waterloo", "Waals-Brabant", new LatLng(50.714690, 4.3991));
+        listMcDonalds.add(mc);         
+                          
+        mc = new McDonald("McDo MESSANCY (Drive)", "Route d'Arlon 220", "6780", "Messancy", "Luxemburg", new LatLng(49.609620, 5.813204));
+        listMcDonalds.add(mc);
+                            
+        mc = new McDonald("McDo MARCHE-EN FAMENNE", "Luikse Steenweg 41", "6900", "Marche-en-Famenne", "Luxemburg", new LatLng(50.235333, 5.344518));
+        listMcDonalds.add(mc);
+                          
+        mc = new McDonald("McDo ANDERLECHT (Cora)", "Drève Olympic 15", "1070", "Anderlecht", "Brussel", new LatLng(50.819360, 4.279048));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo BASCULE (In-Store)", "Chaussé de Waterloo 717", "1180", "Brussel", "Brussel", new LatLng(50.815444, 4.366794));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo BRUXELLES/BRUSSEL (In-Store)", "Rue Neuve 124", "1000", "Brussel", "Brussel", new LatLng(50.854381, 4.3574));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo BOURSE (In-Store)", "Place de la Bourse 3", "1000", "Brussel", "Brussel", new LatLng(50.848655, 4.349291));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo IXELLES (In-Store)", "Chaussée d'Ixelles 21", "1050", "Brussel", "Brussel", new LatLng(50.837371, 4.361952));
+        listMcDonalds.add(mc);
+        
+        mc = new McDonald("McDo DIEST (Drive)", "Leuvensesteenweg 86", "3290", "Diest", "Vlaams Brabant", new LatLng(50.975538, 5.041784));
+        listMcDonalds.add(mc);        
+        
+        mc = new McDonald("McDo HALLE (Drive)", "De Maeghtlaan 102", "1500", "Halle", "Vlaams Brabant", new LatLng(50.736977, 4.231634));
+        listMcDonalds.add(mc);         
+        mc = new McDonald("McDo KAMPENHOUT (Drive)", "Mechelsesteenweg 40", "1910", "Kampenhout", "Vlaams Brabant", new LatLng(50.959433, 4.58719));
+        listMcDonalds.add(mc);
+                 
+        mc = new McDonald("McDo LEUVEN (In-Store)", "Kortestraat 7-9", "3000", "Leuven", "Vlaams Brabant", new LatLng(50.878903, 4.700204));
+        listMcDonalds.add(mc);  
+               
+        mc = new McDonald("McDo OVERIJSE (Drive)", "Brusselsesteenweg 406", "3090", "Overijse", "Vlaams Brabant", new LatLng(50.785848, 4.500371));
+        listMcDonalds.add(mc); 
+                
+        mc = new McDonald("McDo SINT-PIETERS-LEEUW (Drive)", "Bergensesteenweg 65", "1600", "Sint-Pieters-Leeuw", "Vlaams Brabant", new LatLng(50.803385, 4.288906));
+        listMcDonalds.add(mc);  
+               
+        mc = new McDonald("McDo GROOT-BIJGAARDEN (Drive)", "Robert Dansaertlaan 11", "1702", "Groot-Bijgaarden", "Vlaams Brabant", new LatLng(50.867109, 4.274291));
+        listMcDonalds.add(mc);       
+                       
+        mc = new McDonald("McDo HASSELT (Drive)", "Schampbergstraat 22", "3511", "Hasselt", "Limburg", new LatLng(50.938795, 5.302053));
+        listMcDonalds.add(mc);   
+                               
+        mc = new McDonald("McDo HOUTHALEN  (Drive)", "Grote Baan 36", "3530", "Houthalen", "Limburg", new LatLng(51.024472, 5.375363));
+        listMcDonalds.add(mc);  
+                              
+        mc = new McDonald("McDo LOMMEL (Drive)", "Buitensingel 50", "3920", "Lommel", "Limburg", new LatLng(51.221881, 5.315632));
+        listMcDonalds.add(mc); 
+                               
+        mc = new McDonald("McDo MAASMECHELEN  (Drive)", "Layensweg 8", "3630", "Maasmechelen", "Limburg", new LatLng(50.984790, 5.700529));
+        listMcDonalds.add(mc);
+                                
+        mc = new McDonald("McDo BILZEN (Drive)", "Tongersestraat 83", "3740", "Bilzen", "Limburg", new LatLng(50.859567, 5.500802));
+        listMcDonalds.add(mc); 
+                               
+        mc = new McDonald("McDo NAMUR (In-Store)", "Avenue de la Gare 11", "5000", "Namen", "Namen", new LatLng(50.468201, 4.864439));
+        listMcDonalds.add(mc); 
     }
 
     public static List<McDonald> getMcDonalds() {
